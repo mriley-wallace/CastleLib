fbf6498b30b58ce2ba528f96649c32744041cfa8
Michael <55700937+mriley-wallace@users.noreply.github.com>
2019-10-21 17:02:22 +0100

    Update 1 - File clean up
    Using .gitignore to ignore unity files.

---
+++ Assets/Scripts/SmoothFollow2D.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+public class SmoothFollow2D : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5.0f;
+    void FixedUpdate()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, (smoothing * 0.001f));
+    }
+}
---
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+public class TopDownCharacterController2D : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}

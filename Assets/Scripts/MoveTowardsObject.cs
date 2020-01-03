f42837161d6c72002b88a709fef556d21bcb2d4f
Midgento <CEO.Midgento@users.noreply.github.com>
2020-01-03 16:01:41 +0000

    Update 0.7.2
    Pause menu script now added and working as it should.  Also uploading two builds of the game to test.

--- Assets/Scripts/MoveTowardsObject.cs
+++ Assets/Scripts/MoveTowardsObject.cs
@@ -10,11 +10,13 @@ public class MoveTowardsObject : MonoBehaviour
     {
         if (target != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);  
         }
     }
     public void SetTarget(Transform newTarget)
     {
         target = newTarget;
     }
+
+
 }
